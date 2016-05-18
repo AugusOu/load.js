d53319f779767862f48c802ec54af3161117bdf3
Augus <augus.ou@gmail.com>
2016-05-18 10:52:23 +0800

    change

--- loadjs.js
+++ loadjs.js
@@ -1,4 +1,4 @@
-/* loading 2016-05-11 by augus.ou
+/* loading 2016-05-18 by augus.ou
 * email:augus.ou@gamil.com
 * */
 ;(function($,window, document,undefined) {
@@ -8,15 +8,22 @@
         this.source = a, this.count = 0, this.total = a.length, this.onload = c.onload, this.prefix = c.prefix || "";
            this.defaults = {
                 'background': '#fff',
-                'color':"#ccc"
+                'color':"#696969",
+                'icon':'http://auguss.top/importemt/icon.png',
+                'bordercolor':'#ccc'
             },
                this.options = $.extend({}, this.defaults,o),
                this.init();
     };
     //定义Load的方法
     Load.prototype.init = function() {
-        var i='<div id="loadbg" style="position: absolute;left: 0%;top: 0%;width: 100%;height: 100%;background: '+this.check(this.options.background)+';background-size: 100% 100%;z-index: 9999"><div id="loadpercent" style="position: absolute;left:50%;top: 50%;width: 10rem;height: 1rem;font-size: 2rem;color: '+this.options.color+';margin: -5rem 0 0 -5rem;"></div></div>';
+        var i=
+            '<div id="loadbg" style="position: absolute;left: 0%;top: 0%;width: 100%;height: 100%;background: '+this.check(this.options.background)+';background-size: 100% 100%;z-index: 9999">' +
+                '<div  style="position: absolute;left:50%;top: 30%;width: 12rem;height: 15.5rem;font-size: 2rem;color: '+this.options.color+';margin: -6rem 0 0 -6rem;">' +
+                    '<div id="top" style="float:left;width: 12rem;height: 12rem;margin:0 auto;"><img style="border-radius: 50%;border:1px '+this.options.bordercolor+' solid;" width="100%" height="100%" src="'+this.options.icon+'"></div>' +
+                    '<div id="loadpercent" style="float: left;width: 12rem;overflow:hidden;text-align:center;height: 3rem;font-size: 3rem;margin-top:0.5rem;color: '+this.options.color+';"></div></div></div>';
         $('body').append(i);
+        console.log('www.loadjs.top to know API');
         var a = this;
         a.source.forEach(function(b) {
             var c = b.replace(/[#\?].*$/, "").substr(b.lastIndexOf(".") + 1).toLowerCase(), d = a.prefix + b;
@@ -61,10 +68,10 @@
     },Load.prototype.check=function (a) {
         var extStart=a.lastIndexOf('.');
         var ext=a.substring(extStart,a.length).toUpperCase();
-        if(ext!='.BMP'&&ext!='.PNG'&&ext!='.GIF'&&ext!='.JPG'&&ext!='.JPEG'&&ext!='.SVG'&&ext!='.GIF'){
+        if(ext!='.BMP'&&ext!='.PNG'&&ext!='.GIF'&&ext!='.JPG'&&ext!='.JPEG'&&ext!='.SVG'){
             return a;
         }else {
-            return 'url('+a+')no-repeat';
+            return 'url('+a+')no-repeat center';
         }
     };
     //在插件中使用Load对象
@@ -78,7 +85,7 @@
                     setTimeout(function() {
                         if (count == total) {
                             var el = $("#loadbg,#loadpercent");
-                            el.css('display','none');
+                           el.fadeOut("slow");
                         }
                     }, 10);
             }
